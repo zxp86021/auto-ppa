a9d0e59d041299d21801316497e5a9ce64a22c32
richegg <zxp86021@gmail.com>
2016-02-16 14:01:45 +0800

    add more ppa

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+#this script is use for ubuntu 14.04
+
+sudo -s <<EOF
+
 dpkg --add-architecture i386 
 apt-add-repository -y ppa:fish-shell/release-2
 add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 add-apt-repository -y ppa:webupd8team/java
 add-apt-repository -y ppa:wine/wine-builds
+add-apt-repository -y ppa:atareao/telegram
 
 apt-get update
 
-apt-get install fish
-apt-get install ubuntu-make
-apt-get install oracle-java8-installer
-apt-get install --install-recommends winehq-staging
+apt-get install -y fish ubuntu-make oracle-java8-installer telegram
+apt-get install -y --install-recommends winehq-staging
+wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i /tmp/google-chrome*.deb
+apt-get --yes --fix-broken install
+
+EOF
